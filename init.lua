c00ebd4b3b4bbf0765d918f617158ee7d418dadb
yellowhatpro <yellowhatpro3119@gmail.com>
2025-03-25 01:38:48 +0530

    feat: format on save, save keymap added

--- init.lua
+++ init.lua
@@ -6,77 +6,80 @@ vim.g.mapleader = " "
 --start
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out,                            "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- plugins
 local plugins = {
 	-- kanagawa color theme
-	{"rebelot/kanagawa.nvim",priority= 1000},
+	{ "rebelot/kanagawa.nvim",           priority = 1000 },
 	-- telescope fuzzy finder
-	{ 
-		"nvim-telescope/telescope.nvim", tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim"}
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" }
 	},
 	-- treesitter syntax highlighting
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 	-- neotree file explorer tree
-	{ 
+	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
-    			"MunifTanjim/nui.nvim",
-  		},
- 	 },
+			"MunifTanjim/nui.nvim",
+		},
+	},
 	-- lualine
 	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = {"nvim-tree/nvim-web-devicons"},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 	-- LSP plugins (mason, mason-lspconfig, nvim-lspconfig)
 	-- mason
-	{"williamboman/mason.nvim"},
+	{ "williamboman/mason.nvim" },
 	--mason-lspconfig (bridges mason with nvim-lspconfig)
-	{"williamboman/mason-lspconfig.nvim"},
+	{ "williamboman/mason-lspconfig.nvim" },
 	-- nvim-lspconfig
-	{"neovim/nvim-lspconfig"},
+	{ "neovim/nvim-lspconfig" },
 }
 
 -- options settings
 local opts = {}
 
-require("lazy").setup(plugins, ops)
+require("lazy").setup(plugins, opts)
 --end
 
 -- keymaps
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', "<leader>ff", builtin.find_files, {desc = "Telescope find files"})
-vim.keymap.set('n', "<leader>fg", builtin.live_grep, {desc = "Telescope find files"})
-vim.keymap.set('n', "<leader>fb", builtin.buffers, {desc = "Telescope buffers"})
-vim.keymap.set('n', "<leader>fh", builtin.help_tags, {desc = "Telescope help tags"})
+vim.keymap.set('n', "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set('n', "<leader>fg", builtin.live_grep, { desc = "Telescope find files" })
+vim.keymap.set('n', "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set('n', "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 -- neotree keymap
-vim.keymap.set('n', "<leader>e", "<Cmd>Neotree toggle<CR>",{})
+vim.keymap.set('n', "<leader>e", "<Cmd>Neotree toggle<CR>", {})
+
+vim.keymap.set('n', "<C-s>", "<Cmd> w <CR>", { desc = "Save file" })
 
 -- configs
 
 -- kanagawa config.
 -- We could have also wrapped this in a function, and passed to config parameter in the kanagawa table in plugins table.
 require("kanagawa").setup({
-	commentStyle = {italic = true },
-	keywordStyle = {bold = true},
+	commentStyle = { italic = true },
+	keywordStyle = { bold = true },
 	transparent = false,
 	dimInactive = false,
 	terminalColors = true,
@@ -84,7 +87,7 @@ require("kanagawa").setup({
 	background = {
 		dark = "wave",
 		light = "lotus"
-		},
+	},
 })
 
 -- treesitter config.
@@ -92,8 +95,8 @@ local configs = require("nvim-treesitter.configs")
 configs.setup({
 	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "cpp", "javascript", "html" },
 	sync_install = false,
-	highlight = {enable = true},
-	indent = {enable = true},
+	highlight = { enable = true },
+	indent = { enable = true },
 })
 
 -- lualine config.
@@ -110,7 +113,7 @@ require("mason").setup()
 
 --mason-lspconfig
 require("mason-lspconfig").setup({
-	ensure_installed = {"lua_ls", "clangd"} -- contains lsp server names for different languags
+	ensure_installed = { "lua_ls", "clangd" } -- contains lsp server names for different languags
 })
 
 -- lspconfig
@@ -121,8 +124,8 @@ lspconfig.lua_ls.setup({})
 
 -- setup for clangd
 lspconfig.clangd.setup({
-	cmd = {"clangd"},
-	filetypes = {"c", "cpp"},
+	cmd = { "clangd" },
+	filetypes = { "c", "cpp" },
 	root_pattern = {
 		".clangd",
 		".git"
@@ -133,3 +136,4 @@ lspconfig.clangd.setup({
 --apply the colorscheme
 -- equivalent to vim.cmd("colorscheme kanagawa")
 vim.cmd.colorscheme "kanagawa"
+vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]]) -- Format before saving the file
