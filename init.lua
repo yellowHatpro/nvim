db197e7efa6610437007bdb374164fb587d5156f
yellowhatpro <yellowhatpro3119@gmail.com>
2025-03-24 22:59:44 +0530

    feat: added colorscheme kanagawa

--- init.lua
+++ init.lua
@@ -17,7 +17,29 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {}
+local plugins = {
+	-- kanagawa color theme
+	{"rebelot/kanagawa.nvim",priority= 1000},
+	}
 local opts = {}
 
 require("lazy").setup(plugins, ops)
+
+-- kanagawa config.
+-- We could have also wrapped this in a function, and passed to config parameter in the kanagawa table in plugins table.
+require("kanagawa").setup({
+	commentStyle = {italic = true },
+	keywordStyle = {bold = true},
+	transparent = false,
+	dimInactive = false,
+	terminalColors = true,
+	theme = "wave",
+	background = {
+		dark = "wave",
+		light = "lotus"
+		},
+	})
+
+--apply the colorscheme
+-- equivalent to vim.cmd("colorscheme kanagawa")
+vim.cmd.colorscheme "kanagawa"
