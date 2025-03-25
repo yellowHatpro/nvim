aee9e37ac8493c83ad86be0de1d23762d55cda29
yellowhatpro <yellowhatpro3119@gmail.com>
2025-03-26 01:47:47 +0530

    feat: tab completions on suggestions

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- vim global variables
 vim.g.mapleader = " "
-
 -- initialize lazy plugin manager.
 
 --start
@@ -157,8 +156,10 @@ require("mason-lspconfig").setup({
 
 -- Autocompletions configs.
 require("luasnip.loaders.from_vscode").lazy_load()
+local luasnip = require("luasnip")
 local cmp = require("cmp")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 cmp.setup({
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
@@ -177,7 +178,39 @@ cmp.setup({
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
 		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<Tab>'] = cmp.mapping(function(fallback)
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0 and
+				    vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") ==
+				    nil
+			end
+			if cmp.visible() then
+				cmp.select_next_item()
+				-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+				-- this way you will only jump inside the snippet region
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
 	}),
+	completion = {
+		completeopt = "menu,menuone,noinsert,noselect,preview",
+	},
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
 		{ name = 'luasnip' }, -- For luasnip users.
